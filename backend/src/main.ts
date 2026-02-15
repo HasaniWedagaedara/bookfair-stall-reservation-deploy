4b1eebffdbdf6b661cec1d7d8f546a642a8c42e3
Hasani Madhumali <hasaniwedagedara123@gmail.com>
2026-02-16 01:27:08 +0530

    fixed:frontend url adding issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   // Enable CORS for frontend
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'], // Your frontend URL
+    origin: [process.env.FRONTEND_URL || 'http://localhost:3001'], // Your frontend URL
     credentials: true, // Allow cookies
   });
 
